3fbdc33145255c804eeca54857b07a7eeb485e00
Rehmat <rehmat@5683.me>
2022-12-29 18:03:21 +0530

    Updated

---
+++ README.md
@@ -0,0 +1,3 @@
+# utils/promise.js
+
+Read here: <https://animatedcreativity.com/code/node-js/ac-bundle-module/modules/ac-bundle-module-utils-promise/>
---
+++ utils/promise.js
@@ -0,0 +1,22 @@
+exports = module.exports = exports = module.exports = function() {
+  var mod = {
+    wrap: function(value, p) {
+      if (typeof value === "object") {
+        p = value;
+        value = "data";
+      }
+      return new Promise(function(resolve, reject) {
+        p.then(function(d) {
+          var data = {error: undefined};
+          data[value] = d;
+          resolve(data);
+        }).catch(function(e) {
+          var data = {error: e};
+          data[value] = undefined;
+          resolve(data);
+        });
+      });
+    }
+  };
+  return mod;
+}
